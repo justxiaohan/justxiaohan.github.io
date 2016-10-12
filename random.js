72607136cb56631ca824f67b4c6a06f2e1da8679
Xiaohan Zhang <xiaohanzhang@Xiaohans-MacBook-Pro.local>
2016-10-12 08:45:01 -0700

    changed name to Dennis

--- random.js
+++ random.js
@@ -1,4 +1,4 @@
-var currently = ['eating a baked potato', 'breaking code', 'sleeping', 'memorizing Drake lyrics'];
+var currently = ['eating a baked potato', 'breaking code', 'breaking hearts', 'napping', 'memorizing Drake lyrics'];
 
 function displayCurrently() {
 	return currently[Math.floor(Math.random() * currently.length)];
