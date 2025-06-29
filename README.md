e1a772b8c1528b23d2e55c7afee5e29880aae152
Xiaohan Zhang <xiaohan@only-child.local>
2025-06-29 08:31:57 -0700

    a fresh start

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Playing around with Jekyll and the sorts. Smooth and buttery, always. COUNT ME!
+# a new dayyyyyyyy
--- random.js
+++
@@ -1,7 +0,0 @@
-var currently = ['eating a loaded baked potato', 'breaking code', 'breaking hearts', 'napping', 'memorizing Drake lyrics'];
-
-function displayCurrently() {
-	return currently[Math.floor(Math.random() * currently.length)];
-}
-
-$('#doing').append(' ' + displayCurrently());
